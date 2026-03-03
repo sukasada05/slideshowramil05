c14006f6f5f2dc85882e9fd8c365c1e9aace2ae1
sukasada05 <sukasada1609.05@gmail.com>
2026-03-03 14:18:34 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,10 @@
 const CACHE_NAME = 'koramil-cache-v1';
-const urlsToCache = [
-  'index.html',
-  'manifest.json'
-];
+const urlsToCache = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
